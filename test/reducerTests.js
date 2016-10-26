a5e593ab7dc63b153ad0fcdea5adf0abc9c3eac4
James Wright <james@jamesswright.co.uk>
2016-10-26 22:25:45 +0100

    Tests for getNumber

--- test/reducerTests.js
+++ test/reducerTests.js
@@ -4,7 +4,7 @@ import { getNumber } from '../src/reducer';
 
 describe('the reducer tests', function () {
     describe('the getNumber function', function () {
-        it('should return the current guess incremented by one if the guess is correct and the type is min', function () {
+        it('should return the current guess incremented by one if the guess is correct, the guess is for a min value, and the update type is min', function () {
             const currentGuess = {
                 isHigher: true,
                 number: 1
@@ -13,6 +13,48 @@ describe('the reducer tests', function () {
             const isCorrectGuess = true;
             const isMinUpdate = true;
             const expectedNumber = 2;
+            const actualNumber = getNumber(currentGuess, isCorrectGuess, isMinUpdate);
+
+            expect(actualNumber).to.equal(expectedNumber);
+        });
+
+        it('should return the current guess if the guess is incorrect, the guess is for a max value, and the update type is min', function () {
+            const currentGuess = {
+                isHigher: false,
+                number: 1
+            };
+
+            const isCorrectGuess = false;
+            const isMinUpdate = true;
+            const expectedNumber = 1;
+            const actualNumber = getNumber(currentGuess, isCorrectGuess, isMinUpdate);
+
+            expect(actualNumber).to.equal(expectedNumber);
+        });
+
+        it('should return the current guess decremented by one if the guess is correct, the guess is for a max value, and the update type is max', function () {
+            const currentGuess = {
+                isHigher: false,
+                number: 1
+            };
+
+            const isCorrectGuess = true;
+            const isMinUpdate = false;
+            const expectedNumber = 0;
+            const actualNumber = getNumber(currentGuess, isCorrectGuess, isMinUpdate);
+
+            expect(actualNumber).to.equal(expectedNumber);
+        });
+
+        it('should return the current guess if the guess is incorrect, the guess is for a min value, and the update type is max', function () {
+            const currentGuess = {
+                isHigher: true,
+                number: 1
+            };
+
+            const isCorrectGuess = false;
+            const isMinUpdate = false;
+            const expectedNumber = 1;
             const actualNumber = getNumber(currentGuess, isCorrectGuess, isMinUpdate);
 
             expect(actualNumber).to.equal(expectedNumber);
